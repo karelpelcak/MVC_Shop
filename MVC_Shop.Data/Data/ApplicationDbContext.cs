b9d40523cebaa2ccec216cc767752e6f00e53a33
Pupa's dev <kaja.pelcak@seznam.cz>
2024-03-06 11:14:42 +0100

    working on setup

---
+++ MVC_Shop.Data/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using MVC_Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Shop.Data.Data
+{
+    internal class ApplicationDbContext: IdentityDbContext<ApplicationUser>
+    {
+    }
+}
--- MVC_Shop/obj/Debug/net8.0/MVC_Shop.AssemblyInfo.cs
+++ MVC_Shop/obj/Debug/net8.0/MVC_Shop.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC_Shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bff0f62436f2f8e20d70706c579e1fdf4bc659d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC_Shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC_Shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
