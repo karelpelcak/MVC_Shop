3bff0f62436f2f8e20d70706c579e1fdf4bc659d
Pupa's dev <kaja.pelcak@seznam.cz>
2024-03-06 11:10:22 +0100

    Setup

---
+++ MVC_Shop/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using MVC_Shop.Models;
+using System.Diagnostics;
+
+namespace MVC_Shop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
